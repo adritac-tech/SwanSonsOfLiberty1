b7b8175957797f0ab2a2fab0b6d46a434b18abac
florayuan18 <flora.yuan18@gmail.com>
2022-05-31 17:25:04 -0700

    ratings page but stars aren't showing up :/

---
+++ src/main/java/com/nighthawk/csa/mvc/ratings/StarRatingController.java
@@ -0,0 +1,24 @@
+package com.nighthawk.csa.mvc.ratings;
+
+import com.nighthawk.csa.mvc.database.ModelRepository;
+import com.nighthawk.csa.mvc.database.person.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.List;
+
+// Built using article: https://docs.spring.io/spring-framework/docs/3.2.x/spring-framework-reference/html/mvc.html
+// or similar: https://asbnotebook.com/2020/04/11/spring-boot-thymeleaf-form-validation-example/
+@Controller
+public class StarRatingController {
+    @GetMapping("/starrating")
+    public String register() {
+        return "starrating";
+    }
+}
